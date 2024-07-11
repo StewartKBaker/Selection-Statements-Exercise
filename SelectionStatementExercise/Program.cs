799414cb45863864e9e52bc1686a9dc122b04337
StewartBaker <bakrstewart16@gmail.com>
2024-07-11 10:01:54 -0500

    finished exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -5,6 +5,34 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Random random = new Random();
+            int favNumber = random.Next(1, 100);
+            
+            bool guessedCorrectly = false;
+
+            while (!guessedCorrectly)
+            {
+                Console.WriteLine("Guess my Favorite number between 1 and 100!");
+                var userInput = int.Parse(Console.ReadLine());
+                
+                if (userInput < favNumber && userInput < 101)
+                {
+                    Console.WriteLine("Too Low, Try Again");
+                }
+                else if (userInput > favNumber && userInput < 101)
+                {
+                    Console.WriteLine("Too High, Try Again");
+                }
+                else if (userInput == favNumber && userInput < 101)
+                {
+                    Console.WriteLine("You got it! Congratulations!");
+                    guessedCorrectly = true;
+                } else 
+                {
+                    Console.WriteLine("That number is too high, try to stay under 101.");
+                }
+            }
+            
         }
     }
 }
